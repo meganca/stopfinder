2501fa06d6b7b80d5d8347ea5ebad27d0f2b2567
Megan Campbell <meganca@helsinki.cs.washington.edu>
2014-07-16 16:10:13 -0700

    fixed image resize and added better defaults to rep table

--- db/migrate/013_add_badges_to_users.rb
+++ db/migrate/013_add_badges_to_users.rb
@@ -1,7 +1,7 @@
 class AddBadgesToUsers < ActiveRecord::Migration
   def self.up
-    add_column "users", "title", :string
-    add_column "users", "badges", :string
+    add_column "users", "title", :string, default: ""
+    add_column "users", "badges", :string, default: ""
   end
   
   def self.down
